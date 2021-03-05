3a649c339c8ec2c6c14a2b3a9e0dbb8470d8ea5a
Aleksandr Pankin <00010088@wiut.uz>
2021-03-05 16:53:36 +0500

    2 commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const app = require()
+app.set('view engine', 'pug')
+app.get('/', (req,res)=>){
+
+}
+
+app.listen(2000, err =>{
+	if(err) throw err
+		console.log("App is running ... ")
+})
